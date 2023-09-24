03bc92a06fc4c5608c87857f18e831e2924c98e0
snipz <snipzeryt@gmail.com>
2023-09-24 19:06:19 +0100

    24/09/2023

--- main/index.js
+++ main/index.js
@@ -1,6 +1,8 @@
 // gotta keep those apikeys secret
 require('dotenv').config()
 let DiscordKey = process.env.DiscordApiKey
+let uri = process.env.uri
+
 
 // module imports that are required
 const axios = require('axios')
@@ -8,16 +10,104 @@ const date = require('date-and-time')
 const fs = require('node:fs')
 const http = require('node:http')
 const { Client, GatewayIntentBits, Message, Events } = require('discord.js')
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
+
+
 
 // variable init 
-const youtubeApiUsagePricing = [100]
 const YoutubeUrlApicall = 'https://www.googleapis.com/youtube/v3'
-const ChannelId = 'UCXuqSBlHAE6Xw-yeJA0Tunw'
 // test id UCXuqSBlHAE6Xw-yeJA0Tunw
 // saff id UC4c7omgQTXLLZWfG1glBamg
 
+// mongodb
+const MongoDb_Client = new MongoClient(uri, {
+    serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true,}
+});
+
+async function LoadYoutubeApiDB(price) {
+    try {
+      
+      let array = []
+      let temp = []
+      // Connect the client to the server
+      await MongoDb_Client.connect();
+      // Send a ping to confirm a successful connection
+      await MongoDb_Client.db("admin").command({ ping: 1 });
+      console.log("Db connected");
+
+
+      const Database = MongoDb_Client.db("saffbot");
+      const YoutubeApiInfo = Database.collection("YoutubeApiInfo")
+        
+      const cursor = await YoutubeApiInfo.findOne({ UsageCount: { $lt: 10000} });
+
+      temp = cursor.UsageCount
+      temp = temp.map(Number);
+      console.log(temp);
+      let newUsage = temp+price
+
+      await YoutubeApiInfo.updateOne({YoutubeApiKey:`${cursor.YoutubeApiKey}`}, { $set: { UsageCount: `${newUsage}` }})
+      // Print a message if no documents were found
+  
+      // Print returned documents
+      array.push(cursor.YoutubeApiKey, cursor.ApiName, cursor.UsageCount)
+      return array
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await MongoDb_Client.close();
+    }
+}
+async function UpdateYoutubeApiUsage(newUsage, YoutubeApiInfoold) {
+    try {
+      // Connect the client to the server
+      await MongoDb_Client.connect();
+      // Send a ping to confirm a successful connection
+      await MongoDb_Client.db("admin").command({ ping: 1 });
+      console.log("Db connected");
+
+      const Database = MongoDb_Client.db("saffbot");
+      const YoutubeApiInfo = Database.collection("YoutubeApiInfo")
+      console.log(newUsage)
+      await YoutubeApiInfo.updateOne({YoutubeApiKey:`${YoutubeApiInfoold[0]}`}, { $set: { UsageCount: `${newUsage}` }})
+
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await MongoDb_Client.close();
+    }
+}
+
+async function LoadDiscordDB() {
+    try {
+        
+        let array = []
+
+        // Connect the client to the server
+        await MongoDb_Client.connect();
+        // Send a ping to confirm a successful connection
+        await MongoDb_Client.db("admin").command({ ping: 1 });
+        console.log("Db connected");
+        const Database = MongoDb_Client.db("saffbot");
+        const DiscordChannels = Database.collection("discordChannels")
+        const cursor = DiscordChannels.find()
+        // Print a message if no documents were found
+        if ((await DiscordChannels.countDocuments()) === 0) {
+          console.log("No documents found!");
+        }
+    
+        // Print returned documents
+        for await (const doc of cursor) {
+          array.push(doc.DiscordChannelId, doc.YoutubeId, doc.LastStreamUrl)
+        }
+        return array
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await MongoDb_Client.close();
+      }
+}
+
+
 // discordapi setup
-const client = new Client({
+const Discord_Client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -27,64 +117,39 @@ const client = new Client({
         GatewayIntentBits.DirectMessages]
 });
 
-client.login("MTE0ODk4NzY2Nzk4ODYyNzQ5Ng.GVj3f7.9NkeajWUoVlvTd7jP9gECjplMhR1tCvyTOjOvQ")
+Discord_Client.login("MTE0ODk4NzY2Nzk4ODYyNzQ5Ng.GVj3f7.9NkeajWUoVlvTd7jP9gECjplMhR1tCvyTOjOvQ")
 
-client.once(Events.ClientReady, c => {
+Discord_Client.once(Events.ClientReady, c => {
     console.log(`Saffbot alive.`)
 })
-
-// checking api usage count
-const ApiParsing = () => {
-    const YoutubeApiInfojson = JSON.parse(fs.readFileSync('./YoutubeApiInfo.json', { encoding: 'utf8'}))
-    const youtubeApiKeys = YoutubeApiInfojson.YoutubeApiKeys
-    const youtubeApiUsage = YoutubeApiInfojson.youtubeUsageCounts
-
-    console.log('--------------')
-    //checks for an api which does not have maximum usage count
-    for(let i = 0; i != 3; i++){
-        if(youtubeApiUsage[i] != '10000'){
-            console.log(`Api usage count: ${youtubeApiUsage[i]}`)
-            console.log('--------------')
-            return (youtubeApiKeys[i]).split('¬')
-        }
-    }
-}
- 
-    const YoutubeCountUpdate = (youtubeApiKeys, PricingCount) => {
-    const YoutubeApiInfoJson = fs.readFileSync('./YoutubeApiInfo.json', {encoding: 'utf8'})
-    let oldUsageCount = YoutubeApiInfoJson.youtubeUsageCountsvs[youtubeApiKeys[2]]
-    console.log(YoutubeApiInfoJson)
-
-
-
-    fs.writeFileSync('./test.json', `hello world`)
-    console.log("updated successfully.");
-
-}
 
 // Live Checking,
 const Live = async() => {
     try {
 
+        let price = 100
         // parsing youtube api keys
-        const YoutubeApiInUse = ApiParsing()
+        const YoutubeApiInUse = await LoadYoutubeApiDB(price)
         // discord channel id parsing
-        const discordChannelId = JSON.parse(fs.readFileSync('./DiscordInfo.json', {encoding: 'utf-8'}))
+        const discordChannel = await LoadDiscordDB()
         // youtube api searching
-        const response = await axios.get(`${YoutubeUrlApicall}/search?part=snippet&channelId=${ChannelId}&channelType=any&eventType=live&order=relevance&safeSearch=safeSearchSettingUnspecified&type=video&key=${YoutubeApiInUse[1]}`)
+        const response = await axios.get(`${YoutubeUrlApicall}/search?part=snippet&channelId=${discordChannel[1]}&channelType=any&eventType=live&order=relevance&safeSearch=safeSearchSettingUnspecified&type=video&key=${YoutubeApiInUse[0]}`)
         let streamurl = response.data.items.map((item) => item.id.videoId)
 
         console.log('youtube api Used')
 
+
+        //UpdateYoutubeApiUsage(newUsage, YoutubeApiInUse)
+
+
         // update usage count
-        YoutubeCountUpdate(YoutubeApiInUse, youtubeApiUsagePricing[0])
 
         // saves the url so it doesnt multipost the same stream
         let LastStreamUrl = fs.readFileSync('../LastStreamUrl.txt', { encoding: 'utf8', flag: 'r' })
 
         if(streamurl != '' && LastStreamUrl != streamurl){
             // discord channel the announcement is send in
-            const channel = client.channels.cache.get(discordChannelId.DiscordChannelId[0]);
+            const channel = Discord_Client.channels.cache.get(discordChannel[0]);
             channel.send(`<@&1093184251979112539> Saff is now live: https://www.youtube.com/watch?v=${streamurl}`);
 
             console.log('Live status posted')
