9122ddb802e60431aa0a19f833a0ad37e9386620
snipz <snipzeryt@gmail.com>
2023-09-30 01:39:27 +0100

    30/09/2023

--- main/index.js
+++ main/index.js
@@ -16,7 +16,7 @@ const { check } = require('./timecheck.js')
 // variable init 
 const YoutubeUrlApicall = 'https://www.googleapis.com/youtube/v3'
 
-// test id UCXuqSBlHAE6Xw-yeJA0Tunw
+// test id UCSJ4gkVC6NrvII8umztf0Ow
 // saff id UC4c7omgQTXLLZWfG1glBamg
 
 // mongodb
@@ -33,7 +33,7 @@ async function UpdateDataToDB(DB, UsageCount, Price, YoutubeapiKey, GuildId, Dis
     await MongoDb_Client.connect();
     // Send a ping to confirm a successful connection
     await MongoDb_Client.db("admin").command({ ping: 1 });
-    console.log("Db connected");
+    console.log("Updating Data to Db");
     
     const Database = MongoDb_Client.db("saffbot");
     const Collection = Database.collection(DB)
@@ -41,22 +41,31 @@ async function UpdateDataToDB(DB, UsageCount, Price, YoutubeapiKey, GuildId, Dis
 
     if( DB == 'YoutubeApiInfo'){
     
-      temp.push(UsageCount)
-      temp = temp.map(Number)
-      let newUsage = temp[0]+Price
-
+  
+      let newUsage = UsageCount+Price
+      console.log(newUsage)
       await Collection.updateOne({YoutubeApiKey:`${YoutubeapiKey}`}, { $set: { UsageCount: newUsage }})
     }
     if(DB == 'discordChannels' && DiscordUpdateType == 'Guild id'){
-      console.log('Insert guild')
+      console.log('Insertguild')
       await Collection.insertOne({DiscordChannelId:'', YoutubeId:'', LastStream:'', DiscordGuildId:`${GuildId}`})
     }
-    if(DB == 'discordChannels' && DiscordUpdateType == 'Channel id'){
-      console.log('updateChannel')
-      await Collection.updateOne({DiscordGuildId:`${GuildId}`}, {$set:  {DiscordChannelId:`${DiscordChannel}`}})
+    //if(DB == 'discordChannels' && DiscordUpdateType == 'Channel id'){
+    //  console.log('updateChannel')
+    //  await Collection.updateOne({DiscordGuildId:`${GuildId}`}, {$set:  {DiscordChannelId:`${DiscordChannel}`}})
+    //}
+    //if(DB == 'discordChannels' && DiscordUpdateType == 'Youtube id'){
+    //  console.log('updateYoutube')
+    //  await Collection.updateOne({DiscordGuildId:`${GuildId}`}, {$set:  {YoutubeId:`${YoutubeChannelId}`}})
+    //}
+    if(Price == -100){
+      await Collection.updateMany({UsageCount: {$gt: 0}}, { $set: { UsageCount: 0 }})
     }
 
-  } finally {
+  } catch(err) {
+    console.log(err)
+  }
+  finally {
     // Ensures that the client will close when you finish/error       
     await MongoDb_Client.close();
   }
@@ -71,7 +80,7 @@ async function LoadDataFromDB(DB) {
     await MongoDb_Client.connect();
     // Send a ping to confirm a successful connection
     await MongoDb_Client.db("admin").command({ ping: 1 });
-    console.log("Db connected");
+    console.log("Loading Data from Db");
 
 
     const Database = MongoDb_Client.db("saffbot");
@@ -79,25 +88,80 @@ async function LoadDataFromDB(DB) {
       
     if(DB == 'YoutubeApiInfo'){
       const cursor = await Collection.findOne({ UsageCount: { $lt: 10000} });
+
       array.push(cursor.YoutubeApiKey, cursor.ApiName, cursor.UsageCount)
       return array
     }
 
-    if (DB == 'discordChannel'){
-      const cursor = DiscordChannels.find()
-      for await (const doc of cursor) {
-        array.push(doc.DiscordChannelId,doc.guildId, doc.YoutubeId, doc.LastStream)
-      }
+    if (DB == 'discordChannels'){
+      const cursor = Collection.findOne()
+      array.push(cursor.DiscordChannelId,cursor.guildId, cursor.YoutubeId, cursor.LastStream)
+      console.log(array)
       return array
     }
 
-  } finally {
+  } catch(err) {
+    console.log(err)
+  }
+  finally {
     // Ensures that the client will close when you finish/error
     await MongoDb_Client.close();
   }
 }
 
+const YoutubeChannelIds = async(YoutubeChannelUrl) => {
+  try {
 
+    let YoutubeApiInuse = await LoadDataFromDB('YoutubeApiInfo') 
+
+    const response = await axios.get(`${YoutubeUrlApicall}/search?part=id,snippet&maxResults=1&type=channel&q=${YoutubeChannelUrl}&key=${YoutubeApiInuse[0]}`)
+
+    let YoutubeChannelId = response.data.items.map((item) => item.id.channelId)
+
+    console.log(YoutubeChannelId[0])
+    return YoutubeChannelId[0]
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
+// Live Checking,
+const Live = async() => {
+  try {
+
+    // parsing youtube api keys
+      const YoutubeApiInUse = await LoadDataFromDB('YoutubeApiInfo')
+      // discord channel id parsing
+      //const discordChannel = await LoadDataFromDB('discordChannels')
+      //console.log(discordChannel)
+
+      // youtube api searching
+      const response = await axios.get(`${YoutubeUrlApicall}/search?part=snippet&channelId=UC4c7omgQTXLLZWfG1glBamg&channelType=any&eventType=live&order=relevance&safeSearch=safeSearchSettingUnspecified&type=video&key=${YoutubeApiInUse[0]}`)
+      let streamurl = response.data.items.map((item) => item.id.videoId)
+      let UsageCount = YoutubeApiInUse[2]
+      // update usage count
+      await UpdateDataToDB('YoutubeApiInfo', UsageCount, 100, YoutubeApiInUse[0], '', '', '', '', '', '')
+      console.log('youtube api Used')
+
+
+      // saves the url so it doesnt multipost the same stream
+      let LastStreamUrl = fs.readFileSync('../LastStreamUrl.txt', { encoding: 'utf8', flag: 'r' })
+      if(streamurl != '' && LastStreamUrl != streamurl){
+          // discord channel the announcement is send in
+          const channel = Discord_Client.channels.cache.get("1080156472488497253");
+          channel.send(`<@&1093184251979112539> Saff is now live: https://www.youtube.com/watch?v=${streamurl[0]}`);
+
+          console.log('Live status posted')
+          fs.writeFileSync('../LastStreamUrl.txt', `${streamurl}`)
+      }
+
+
+  } catch (err) {
+      console.log(err)
+  }
+}
 
 // discordapi setup
 const Discord_Client = new Client({
@@ -109,71 +173,30 @@ const Discord_Client = new Client({
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.DirectMessages]
 });
-
 Discord_Client.login(DiscordKey)
-
 Discord_Client.once(Events.ClientReady, c => {
     console.log(`Saffbot alive.`)
 })
 
-
-Discord_Client.on('messageCreate', message => {
-  if(message.content == '!setchannel'){
+/*Discord_Client.on('messageCreate', message => {
+  let tempMessage = (message.content).split(' ')
+  if(tempMessage[0] == '!setchannel'){
     message.reply('channel saved')
-    let tempMessage = (message.content).split(' ')
-    console.log(tempmessage)
+
     let channelId = message.channelId
     let guildId = message.guildId
-    console.log(channelId)
-    console.log(guildId)
-    //find youtube channel id
-    let YoutubeChannelId = ''
 
-    UpdateDataToDB('discordChannels', 0, 0, '', guildId, channelId, YoutubeChannelId, '', 'Channel id')
+    //find youtube channel id
+    UpdateDataToDB('discordChannels', 0, 0, '', guildId, channelId, '', '', 'Channel id')
   }
-})
+})*/
 
 Discord_Client.on("guildCreate", message => {
   console.log('Discord Server Joined')
   let guildId = message.guildId
   //                str           int int str             str str str
   UpdateDataToDB('discordChannels', 0, 0, '', guildId, '','','', 'Guild id')
-} )
-
-
-// Live Checking,
-const Live = async() => {
-    try {
-
-        let price = 100
-        // parsing youtube api keys
-        const YoutubeApiInUse = await LoadDataFromDB('')
-        // discord channel id parsing
-        const discordChannel = await LoadDataFromDB('')
-        // youtube api searching
-        const response = await axios.get(`${YoutubeUrlApicall}/search?part=snippet&channelId=${discordChannel[1]}&channelType=any&eventType=live&order=relevance&safeSearch=safeSearchSettingUnspecified&type=video&key=${YoutubeApiInUse[0]}`)
-        let streamurl = response.data.items.map((item) => item.id.videoId)
-
-        console.log('youtube api Used')
-
-        // update usage count
-
-        // saves the url so it doesnt multipost the same stream
-        let LastStreamUrl = fs.readFileSync('../LastStreamUrl.txt', { encoding: 'utf8', flag: 'r' })
-        if(streamurl != '' && LastStreamUrl != streamurl){
-            // discord channel the announcement is send in
-            const channel = Discord_Client.channels.cache.get(discordChannel[0]);
-            channel.send(`<@&1093184251979112539> Saff is now live: https://www.youtube.com/watch?v=${streamurl}`);
-
-            console.log('Live status posted')
-            fs.writeFileSync('../LastStreamUrl.txt', `${streamurl}`)
-        }
-
-
-    } catch (err) {
-        console.log(err)
-    }
-}
+})
 
 
 const delayExecution = (ms) => {
@@ -184,15 +207,27 @@ const delayExecution = (ms) => {
     })
 }
 
+let count = 0
+
 const loop = async () => {
   await delayExecution(2000)
   while (true) {
-    await delayExecution(1000)
+    await delayExecution(4000)
     let current_mins = date.format(new Date, 'mm')
+    let current_Hour = date.format(new Date, 'hh')
     let response = check(current_mins)
 
     if (response == 1){
         console.log('checked')
+        Live()
+    }
+    if (current_Hour == '08' & count>3){
+      await UpdateDataToDB('', 0, -100, '', '', '', '', '', '')
+      console.log('reset usagecount')
+      count++
+    }
+    if (current_Hour == '08' & count==3){
+      count = 0
     }
   }
 }
