9bf9b3297cb934ed1390744eb39f4464730fd22a
snipz <snipzeryt@gmail.com>
2024-04-27 20:00:01 +0100

    v4 27/04/2024

--- index.js
+++ index.js
@@ -3,191 +3,261 @@ const fs = require('node:fs');
 const csv = require('csv-parser');
 require('dotenv').config();
 
+//	variables
+const message_Logger_id = '1198380107409145906';
+const stream_announcement_id = '1178843756603113593';
+const saffId = '1054580330629189733';
+
 // need to keep those api keys secret you know
 let DiscordKey = process.env.DiscordTolken;
 
 // discord setup bullshit
 const Discord_Client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.DirectMessageReactions,
-        GatewayIntentBits.DirectMessageTyping,
-    ],
-    partials: [
-        Partials.Channel, // Required to receive DMs
-    ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.DirectMessageTyping,
+		GatewayIntentBits.GuildPresences,
+	],
+	partials: [
+		Partials.Channel, // Required to receive DMs
+	],
 });
 
 // just logs in to saffbot
 Discord_Client.login(DiscordKey);
 Discord_Client.once(Events.ClientReady, (c) => {
-    console.log(`Saffbot alive.`);
+	console.log(`Saffbot alive.`);
 });
 
 const remove_linebreaks = (str) => {
-    let newstr = '';
+	let newstr = '';
 
-    // Looop and traverse string
-    for (let i = 0; i < str.length; i++)
-        if (!(str[i] == '\n' || str[i] == '\r')) newstr += str[i];
-    return newstr;
+	// Looop and traverse string
+	for (let i = 0; i < str.length; i++) if (!(str[i] == '\n' || str[i] == '\r')) newstr += str[i];
+	return newstr;
 };
 
 const MESSAGELOGGER_DIR = './Messages_logger';
 let Discord_Channel_Array = [];
 const ReadMessageloggerDir = async () => {
-    fs.readdir(MESSAGELOGGER_DIR, (err, files) => {
-        if (err) console.log(err);
-        else {
-            files.forEach((file) => {
-                Discord_Channel_Array.push(file);
-            });
-        }
-    });
+	fs.readdir(MESSAGELOGGER_DIR, (err, files) => {
+		if (err) console.log(err);
+		else {
+			files.forEach((file) => {
+				Discord_Channel_Array.push(file);
+			});
+		}
+	});
 };
 const discordchannelexists = (discordChannelId) => {
-    let found = false;
-    for (let i = 0; i != Discord_Channel_Array.length; i++) {
-        if (discordChannelId + '.csv' == Discord_Channel_Array[i]) {
-            console.log('found');
-            found = true;
-        }
-    }
-    return found;
+	let found = false;
+	for (let i = 0; i != Discord_Channel_Array.length; i++) {
+		if (discordChannelId + '.csv' == Discord_Channel_Array[i]) {
+			console.log('found');
+			found = true;
+		}
+	}
+	return found;
 };
 const CreateChannelcsv = (MessageChannelId) => {
-    if (discordchannelexists(MessageChannelId) != true) {
-        console.log('channel not found');
-        fs.writeFileSync(
-            `${MESSAGELOGGER_DIR}/${MessageChannelId}.csv`,
-            'MessageId,AuthorName,CreatedTimeStamp,Reply,Attachment_url,Content,MentionedUsers,EditedTimeStamp,EditedAttachements,EditedContent,',
-            (err) => {
-                if (err) console.log(err);
-            }
-        );
-        Discord_Channel_Array.push(`${MessageChannelId}.csv`);
-    }
+	if (discordchannelexists(MessageChannelId) != true) {
+		console.log('channel not found');
+		fs.writeFileSync(`${MESSAGELOGGER_DIR}/${MessageChannelId}.csv`, 'MessageId,AuthorName,CreatedTimeStamp,Reply,Attachment_url,Content,MentionedUsers,EditedTimeStamp,EditedAttachements,EditedContent,', (err) => {
+			if (err) console.log(err);
+		});
+		Discord_Channel_Array.push(`${MessageChannelId}.csv`);
+	}
 };
 const SaveMessageTocsv = async (message_raw) => {
-    let Reply = '';
-    let attachment_array = message_raw.attachments.map((x) =>
-        JSON.stringify(x.url)
-    );
-    let attachment_log = '';
-    //checks if array exits has more items than 1
-    if (attachment_array.length > 1) {
-        attachment_log = attachment_array.join('¬');
-    } else if (attachment_array.length == 1) {
-        attachment_log = attachment_array[0];
-    }
+	let Reply = '';
+	let attachment_array = message_raw.attachments.map((x) => JSON.stringify(x.url));
+	let attachment_log = '';
+	//checks if array exits has more items than 1
+	if (attachment_array.length > 1) {
+		attachment_log = attachment_array.join('¬');
+	} else if (attachment_array.length == 1) {
+		attachment_log = attachment_array[0];
+	}
 
-    //checks if the message is a reply to a user
-    if (message_raw.mentions.repliedUser != null) {
-        Reply = `${message_raw.mentions.repliedUser.username} $ ${message_raw.mentions.repliedUser}`;
-    }
-    // checks mentioned users in a message, for later replacement in Content
-    let Mentions_username_array = [];
-    let Mentions_Id_array = [];
-    let mentionedUsers = '';
-    if (message_raw.mentions.users != null) {
-        Mentions_username_array = message_raw.mentions.users.map((x) =>
-            JSON.stringify(x.username)
-        );
-        Mentions_Id_array = message_raw.mentions.users.map((x) =>
-            JSON.stringify(x.id)
-        );
+	//checks if the message is a reply to a user
+	if (message_raw.mentions.repliedUser != null) {
+		Reply = `${message_raw.mentions.repliedUser.username} $ ${message_raw.mentions.repliedUser}`;
+	}
+	// checks mentioned users in a message, for later replacement in Content
+	let Mentions_username_array = [];
+	let Mentions_Id_array = [];
+	let mentionedUsers = '';
+	if (message_raw.mentions.users != null) {
+		Mentions_username_array = message_raw.mentions.users.map((x) => JSON.stringify(x.username));
+		Mentions_Id_array = message_raw.mentions.users.map((x) => JSON.stringify(x.id));
 
-        for (let i = 0; i < Mentions_username_array.length; i++) {
-            mentionedUsers =
-                mentionedUsers +
-                `${Mentions_username_array[i].split('"')[1]}$${
-                    Mentions_Id_array[i].split('"')[1]
-                } ¬ `;
-        }
-    }
+		for (let i = 0; i < Mentions_username_array.length; i++) {
+			mentionedUsers = mentionedUsers + `${Mentions_username_array[i].split('"')[1]}$${Mentions_Id_array[i].split('"')[1]} ¬ `;
+		}
+	}
 
-    // input sanitization
-    // commas break csv files since "comma seperated"
-    let MessageContent = message_raw.content.replace(/,/g, ' ');
-    MessageContent = MessageContent.replace(/"/g, ' ');
+	// input sanitization
+	// commas break csv files since "comma seperated"
+	let MessageContent = message_raw.content.replace(/,/g, ' ');
+	MessageContent = MessageContent.replace(/"/g, ' ');
 
-    // single quote breaks csv files so just going to remove them all
+	// single quote breaks csv files so just going to remove them all
 
-    // line breaks also break csv files need to remove them
-    MessageContent = remove_linebreaks(MessageContent);
+	// line breaks also break csv files need to remove them
+	MessageContent = remove_linebreaks(MessageContent);
 
-    // saves message to csv file of the channel where it was sent
-    fs.appendFile(
-        `${MESSAGELOGGER_DIR}/${message_raw.channelId}.csv`,
-        `\n${message_raw.id},${message_raw.author.username},${message_raw.createdTimestamp},${Reply},${attachment_log},${MessageContent},${mentionedUsers},,,,`,
-        (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-            }
-        }
-    );
+	// saves message to csv file of the channel where it was sent
+	fs.appendFile(`${MESSAGELOGGER_DIR}/${message_raw.channelId}.csv`, `\n${message_raw.id},${message_raw.author.username},${message_raw.createdTimestamp},${Reply},${attachment_log},${MessageContent},${mentionedUsers},,,,`, (err) => {
+		if (err) {
+			console.log(err);
+		} else {
+		}
+	});
+};
+const PostMessage_guild = (message) => {
+	let attachment_array = message.attachments.map((x) => JSON.stringify(x.url));
+	let attachment_log = '';
+	if (attachment_array.length > 1) {
+		attachment_log = attachment_array.join('¬');
+	} else if (attachment_array.length == 1) {
+		attachment_log = attachment_array[0];
+		attachment_log = `< ${attachment_log} >: |`;
+	} else {
+		attachment_log = '';
+	}
+
+	//checks if the message is a reply to a user
+	if (message.mentions.repliedUser != null) {
+		Reply = `${message.mentions.repliedUser.username} $ ${message.mentions.repliedUser}`;
+	} else {
+		Reply = '';
+	}
+
+	// checks mentioned users in a message, for later replacement in Content
+	let Mentions_username_array = [];
+	let Mentions_Id_array = [];
+	let mentionedUsers = '';
+	let content = message.content;
+	if (message.mentions.users != null) {
+		Mentions_username_array = message.mentions.users.map((x) => JSON.stringify(x.username));
+		Mentions_Id_array = message.mentions.users.map((x) => JSON.stringify(x.id));
+
+		for (let i = 0; i < Mentions_username_array.length; i++) {
+			mentionedUsers = mentionedUsers + `${Mentions_username_array[i].split('"')[1]}$${Mentions_Id_array[i].split('"')[1]} ¬ `;
+		}
+	}
+
+	// replys
+	output_reply = [];
+	if (Reply != '') {
+		output_reply = Reply.split(' $ ')[0];
+	}
+	let OutputTimestamp = Math.round(message.createdTimestamp / 1000);
+	let MessageContent = remove_linebreaks(content);
+
+	let mentioned_users_array = mentionedUsers.split(' ¬ ');
+	let mentioned_users_array_split = [];
+	for (let i = 0; i < mentioned_users_array.length - 1; i++) {
+		mentioned_users_array_split.push(mentioned_users_array[i].split('$'));
+	}
+
+	for (let i = 0; i < mentioned_users_array_split.length; i++) {
+		MessageContent = MessageContent.replace(`<@${mentioned_users_array_split[i][1]}>`, `${mentioned_users_array_split[i][0]}`);
+	}
+	MessageContent = MessageContent.replace(`@everyone`, 'everyone');
+	outgoing_log = ' <t:' + OutputTimestamp + ':d> |  ' + attachment_log + ' ' + '< https://discord.com/channels/' + message.guildId + '/' + message.channelId + ' >: | ' + message.author.username + ': ' + MessageContent + ' | Reply: ' + output_reply;
+	if (outgoing_log.length > 2000) {
+		let OverLimit = outgoing_log.length - 2000;
+
+		MessageContent = MessageContent.substring(0, MessageContent.length - OverLimit - 100);
+		MessageContent = MessageContent + ' Message too long';
+		outgoing_log = ' <t:' + OutputTimestamp + ':d> |  ' + attachment_log + ' ' + '< https://discord.com/channels/' + message.guildId + '/' + message.channelId + ' >: | ' + message.author.username + ': ' + MessageContent + ' | Reply: ' + output_reply;
+		// if the messages is still longer then 2000 characters "Message Too long to be sent in discord" is sent, last resort in case of length
+		if (outgoing_log.length > 2000) {
+			outgoing_log = ' <t:' + OutputTimestamp + ':d> Message is too long to be sent in discord';
+		}
+	}
+
+	if (outgoing_log != '') {
+		const Logs_channel = Discord_Client.channels.cache.get(message_Logger_id);
+		console.log(outgoing_log);
+		Logs_channel.send({
+			content: outgoing_log,
+			flags: [4096],
+		});
+		return;
+	}
 };
 
 ReadMessageloggerDir();
 
-const COMPLAINTSLOGGER_DIR = './Complaints_logger';
-let Discord_Dm_Array = [];
-const ReadComplaintsloggerDir = async () => {
-    fs.readdir(COMPLAINTSLOGGER_DIR, (err, files) => {
-        if (err) console.log(err);
-        else {
-            files.forEach((file) => {
-                Discord_Dm_Array.push(file);
-            });
-        }
-    });
-};
-const discordDmexists = (discordChannelId) => {
-    let found = false;
-    for (let i = 0; i != Discord_Dm_Array.length; i++) {
-        if (discordChannelId + '.csv' == Discord_Dm_Array[i]) {
-            console.log('found');
-            found = true;
-        }
-    }
-    return found;
-};
-const CreateDmcsv = (MessageChannelId) => {
-    if (discordDmexists(MessageChannelId) != true) {
-        console.log('channel not found');
-        fs.writeFileSync(
-            `${COMPLAINTSLOGGER_DIR}/${MessageChannelId}.csv`,
-            'MessageId,AuthorName,CreatedTimeStamp,Reply,Attachment_url,Content,MentionedUsers,EditedTimeStamp,EditedAttachements,EditedContent,',
-            (err) => {
-                if (err) console.log(err);
-            }
-        );
-        Discord_Dm_Array.push(`${MessageChannelId}.csv`);
-    }
-};
-
-ReadComplaintsloggerDir();
-
 Discord_Client.on(Events.MessageCreate, async (message) => {
-    //console.log(Discord_Channel_Array);
-    if (message.guildId == null && message.author.bot == false) {
-        console.log('this is a dm sent by a human');
-        CreateDmcsv(message.channelId);
-    }
-    if (message.guildId != null && message.author.bot == false) {
-        console.log('this is a discord server sent by a human');
-        CreateChannelcsv(message.channelId);
-        SaveMessageTocsv(message);
-    }
+	if (message.guildId == null && message.author.bot == false && message.type != 18) {
+		console.log('dm');
+	}
 
-    Discord_Channel_Array = [];
-    Discord_Dm_Array = [];
-    await ReadMessageloggerDir();
-    await ReadComplaintsloggerDir();
+	if (message.guildId != null && message.author.bot == false && message.type != 18) {
+		CreateChannelcsv(message.channelId);
+		SaveMessageTocsv(message);
+		PostMessage_guild(message);
+	}
+
+	Discord_Channel_Array = [];
+	Discord_Dm_Array = [];
+});
+
+Discord_Client.on(Events.PresenceUpdate, (oldPresence, newPresence) => {
+	if (newPresence.userId == saffId) {
+		if (newPresence.activities[0] != null) {
+			if (newPresence.activities[0].type == 1) {
+				let LastStreamUrl = fs.readFileSync('./LastStreamUrl.txt', {
+					encoding: 'utf8',
+					flag: 'r',
+				});
+				if (newPresence.activities[0].url != LastStreamUrl) {
+					const channel = Discord_Client.channels.cache.get(stream_announcement_id);
+					channel.send(`<@&1093184251979112539> Saff is now live: ${newPresence.activities[0].url}`);
+					fs.writeFileSync('./LastStreamUrl.txt', `${newPresence.activities[0].url}`);
+				}
+			}
+		}
+	}
+});
+
+Discord_Client.on(Events.MessageCreate, (message) => {
+	Hasrole = message.member.roles.cache.map((role) => role.id).join(', ');
+	
+	if ((message.content).toLowerCase() == '!shill') {
+		let shill_message_content = fs.readFileSync("./shill_message.txt", {encoding: "utf8", flag: "r",});
+		if (Hasrole.includes('1080158505325051925') || Hasrole.includes('1080157304827154522') || message.author.id == '492307016795029515') {
+			message.reply(shill_message_content);
+		} else {
+			console.log('You do not have perms')
+		}
+	}
+});
+
+Discord_Client.on(Events.MessageCreate, (message) => {
+	Hasrole = message.member.roles.cache.map((role) => role.id).join(', ');
+	if ((message.content.toLowerCase()).includes('!shillchange')) {
+		if (Hasrole.includes('1080158505325051925') || Hasrole.includes('1080157304827154522') || message.author.id == '492307016795029515') {
+			let content = message.content
+
+			content = content.split(' ')
+			content.splice(0,1)
+			content = content.join(' ')
+
+			fs.writeFileSync("./shill_message.txt", `${content}`)
+			
+			message.reply('changed');
+		} else {
+			console.log('You do not have perms')
+		}
+	}
 });
